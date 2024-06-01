3502d9a89d2b0af4fd5f82e2a3fff01baecdd475
Hacer <yaman.hacer@outlook.com>
2024-06-01 16:48:56 +0300

    app dark and light mode added

--- app/src/main/java/com/baitent/movies_imdb_kotlin_mvvm/view/MoviesFragment.kt
+++ app/src/main/java/com/baitent/movies_imdb_kotlin_mvvm/view/MoviesFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
 import com.baitent.movies_imdb_kotlin_mvvm.R
 
 class MoviesFragment : Fragment() {
@@ -19,6 +21,12 @@ class MoviesFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
     }
 
 }
