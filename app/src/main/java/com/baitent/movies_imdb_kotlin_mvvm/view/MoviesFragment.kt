a689aefcd5a24d9711ba1d6c56291b576e7daf29
Hacer <yaman.hacer@outlook.com>
2024-06-01 15:55:02 +0300

    Initial commit

---
+++ app/src/main/java/com/baitent/movies_imdb_kotlin_mvvm/view/MoviesFragment.kt
@@ -0,0 +1,24 @@
+package com.baitent.movies_imdb_kotlin_mvvm.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.baitent.movies_imdb_kotlin_mvvm.R
+
+class MoviesFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+}
