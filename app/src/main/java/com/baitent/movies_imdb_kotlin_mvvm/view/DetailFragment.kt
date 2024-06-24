0b5d1ebd673cf012c151c7d0c440b3a3e67d6224
Hacer <yaman.hacer@outlook.com>
2024-06-24 11:04:20 +0300

    detail view

--- app/src/main/java/com/baitent/movies_imdb_kotlin_mvvm/view/DetailFragment.kt
+++ app/src/main/java/com/baitent/movies_imdb_kotlin_mvvm/view/DetailFragment.kt
@@ -3,12 +3,20 @@ package com.baitent.movies_imdb_kotlin_mvvm.view
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Observer
 import com.baitent.movies_imdb_kotlin_mvvm.R
 import com.baitent.movies_imdb_kotlin_mvvm.viewmodel.DetailViewModel
@@ -23,6 +31,7 @@ class DetailFragment : Fragment() {
     private lateinit var movieTitle: TextView
     private lateinit var movieRating: TextView
     private lateinit var movieDescription: TextView
+    private lateinit var toolbar: Toolbar
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,6 +50,10 @@ class DetailFragment : Fragment() {
         movieRating = view.findViewById(R.id.textView2)
         movieDescription = view.findViewById(R.id.textView3)
 
+        toolbar = view.findViewById(R.id.detail_toolbar)
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        (activity as AppCompatActivity).supportActionBar?.title = "Movie Detail"
+
         val args = DetailFragmentArgs.fromBundle(requireArguments())
         val movieId = args.movieId
 
@@ -51,14 +64,49 @@ class DetailFragment : Fragment() {
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val menuHost: MenuHost = requireActivity()
+
+        menuHost.addMenuProvider(
+            object : MenuProvider {
+                override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                    menuInflater.inflate(R.menu.detail_menu, menu)
+                }
+
+                override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                    return when (menuItem.itemId) {
+
+                        R.id.action_fav -> {
+                            // Settings item seçildiğinde yapılacak işlemler
+                            true
+                        }
+
+                        else -> false
+                    }
+                }
+            },
+            viewLifecycleOwner,
+            Lifecycle.State.CREATED
+        )
+    }
+
     private fun observeViewModel() {
         detailViewModel.movieDetail.observe(viewLifecycleOwner, Observer { movie ->
             movie?.let {
                 progressBar.visibility = View.GONE
-                Glide.with(this).load(movie.poster_path).into(moviePoster)
+                //Glide.with(this).load(movie.poster_path).into(moviePoster)
                 movieTitle.text = movie.title
                 movieRating.text = movie.vote_average.toString()
                 movieDescription.text = movie.overview
+
+                val posterPath = movie.poster_path
+                val posterUrl = "https://image.tmdb.org/t/p/w500$posterPath"
+
+                Glide.with(this)
+                    .load(posterUrl)
+                    .into(moviePoster)
             }
         })
 
